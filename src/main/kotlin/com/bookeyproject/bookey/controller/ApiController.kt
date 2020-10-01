e34d9537e8d88ed82a13c6b1299aa493701a9198
김준수 <junsoo.kim@nhn.com>
2020-10-01 22:57:46 +0900

    logback

--- src/main/kotlin/com/bookeyproject/bookey/controller/ApiController.kt
+++ src/main/kotlin/com/bookeyproject/bookey/controller/ApiController.kt
@@ -1,6 +1,5 @@
 package com.bookeyproject.bookey.controller
 
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
