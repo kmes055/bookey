09bb9655ccb737a5c372c2787facfa201d8ff66d
jun soo kim <kmes055@naver.com>
2021-06-25 23:08:33 +0900

    단일 item도 body로 들어가도록 변경

--- src/main/kotlin/com/bookeyproject/bookey/web/domain/StandardResponse.kt
+++ src/main/kotlin/com/bookeyproject/bookey/web/domain/StandardResponse.kt
@@ -2,13 +2,11 @@ package com.bookeyproject.bookey.web.domain
 
 import com.bookeyproject.bookey.web.type.ResponseType
 import com.bookeyproject.bookey.web.type.ResponseType.SUCCESS
-import com.fasterxml.jackson.annotation.JsonUnwrapped
 
 data class StandardResponse<T>(
     val success: Boolean = true,
     val resultCode: String = SUCCESS.resultCode,
     val resultMessage: String = SUCCESS.resultMessage,
-    @JsonUnwrapped
     val body: T? = null
 ) {
     constructor(responseType: ResponseType) : this(
